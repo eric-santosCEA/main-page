7a524cabbce9c4767fe845a31b02e160fce6f901
Eric Santos <e.santos@joincea.com>
2021-04-30 12:43:59 -0600

    fixed image position for mobile only, needs media queries for desktop

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
+        <div className="home-container">
             <h1>Home</h1>
+            <div className="image"><img src="/images/city.jpg" alt="city"/></div>
         </div>
     )
 }
