01076a900f5d357843579025b3e756afd6220a6b
Eric Santos <e.santos@joincea.com>
2021-05-07 11:35:08 -0600

    added background logo to home links

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,13 +4,40 @@ const Home = () => {
   return (
     <div className="home-container">
       <div className="btn">
-        <button>
+        <button
+          style={{
+            backgroundImage: 'url(/images/logo.png)',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'fixed',
+            backgroundOrigin: 'center',
+            backgroundSize: '20rem 20rem',
+            backgroundColor: 'red',
+          }}
+        >
           <a href="https://www.solarcea.com/">Roofing</a>
         </button>
-        <button>
+        <button
+          style={{
+            backgroundImage: 'url(/images/logo.png)',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'fixed',
+            backgroundOrigin: 'center',
+            backgroundSize: '20rem 20rem',
+            backgroundColor: '#5cba47',
+          }}
+        >
           <a href="https://www.solarcea.com/">Solar</a>
         </button>
-        <button>
+        <button
+          style={{
+            backgroundImage: 'url(/images/logo.png)',
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'fixed',
+            backgroundOrigin: 'center',
+            backgroundSize: '20rem 20rem',
+            backgroundColor: 'blue',
+          }}
+        >
           <a href="https://www.solarcea.com/">Water</a>
         </button>
       </div>
