84b469658bc857df50d4b3b4b70e4f9dd8d218d4
Eric Santos <e.santos@joincea.com>
2021-05-10 15:37:00 -0600

    added centered icons

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { FaPhone, FaMoneyBill, FaToolbox } from 'react-icons/fa'
 
 const MainNav = () => {
   return (
     <>
       <div className="bottom-nav">
         <Link to="/work" className="btm-link">
+          <FaToolbox size="30px" />
           <h4>Work</h4>
         </Link>
         <Link to="/hiring" className="btm-link">
+          <FaMoneyBill size="30px" />
           <h4>Hiring</h4>
         </Link>
         <Link to="contact" className="btm-link">
+          <FaPhone size="30px" />
           <h4>Contact</h4>
         </Link>
       </div>
