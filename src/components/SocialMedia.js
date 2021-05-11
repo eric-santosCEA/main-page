5ef0cefcf0110f187802f8f49490cdd0b2d372ca
Eric Santos <e.santos@joincea.com>
2021-05-11 12:46:02 -0600

    fixed footer icon margins

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -9,15 +9,24 @@ const SocialMedia = () => {
   return (
     <>
       <div className="social-media">
-        <a href="https://www.facebook.com/">
-          <AiOutlineFacebook size="25px" />
-        </a>
-        <a href="https://www.instagram.com/">
-          <AiOutlineInstagram size="25px" />
-        </a>
-        <a href="https://www.linkedin.com/feed/">
-          <AiOutlineLinkedin size="25px" />
-        </a>
+        <div className="box">
+          <a href="https://www.facebook.com/">
+            <AiOutlineFacebook size="25px" className="icon" />
+            <h6>Facebook</h6>
+          </a>
+        </div>
+        <div className="box">
+          <a href="https://www.instagram.com/">
+            <AiOutlineInstagram size="25px" className="icon" />
+            <h6>Instagram</h6>
+          </a>
+        </div>
+        <div className="box">
+          <a href="https://www.linkedin.com/feed/">
+            <AiOutlineLinkedin size="25px" className="icon" />
+            <h6>LinkedIn</h6>
+          </a>
+        </div>
       </div>
     </>
   )
